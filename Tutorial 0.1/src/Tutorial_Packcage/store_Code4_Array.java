6fffdccda0b48f9cb1949e8b0720d679e1e4d3ae
christiani1 <christiani1@091DS-1182-29.student.cssd.ab.ca>
2019-01-09 12:24:29 -0700

    py game finished

--- Tutorial 0.1/src/Tutorial_Packcage/store_Code4_Array.java
+++ Tutorial 0.1/src/Tutorial_Packcage/store_Code4_Array.java
@@ -12,7 +12,7 @@ public class store_Code4_Array {
 	static final double TAX = 0.05;//tax that needs to be paid
 	static int totalQty = 0;//counter - accumulator
 	static String[] productArray = {"Supreme hoodie", "Bape hoodie", "Louis V bag", "Gucci flip flops", "Balenciaga shoes", "Off-White belt",
-			"Yeezy 350 V2", "Nike AirMax", "Adidas NMD", "Nike Vapormax"};//array for different products
+			"Yeezy 350 V2", "Nike AirMax", "Adidas NMD", "Nike Vapormax", };//array for different products
 	
 	static double[] priceArray = { 1000.0, 1500.50, 2500.0, 1250.50, 2550.50, 3500.0, 1200.0, 230.0, 250.50, 400.0 }; //array for the prices of the items 
 	static int[] quantity = new int [10];
@@ -25,10 +25,6 @@ public class store_Code4_Array {
 	}	
 	 
 	public static void menu() {
-
-		
-
-
 		
 		System.out.println("What is your name? ");//creating a prompt for the user to enter in name
 		String name = scanWords.nextLine();//declares name variable and stores it 
@@ -41,6 +37,7 @@ public class store_Code4_Array {
 		do {//do while loop start
 			
 		list();//calls list funciton 
+		System.out.println("If you are done buying, press 0\n");
 		
 				itemNum = scanNums.nextInt();//scans what item they want
 
@@ -55,45 +52,63 @@ public class store_Code4_Array {
 			if(itemNum == 1) {//if user picks 1, they choose that item
 				price += calcCost(priceArray[itemNum - 1], qty);//calculates the total cost 
 				quantity[itemNum-1] += qty;
-				System.out.println("You chose to buy " + quantity[0] + " Supreme bogo tee");
+				System.out.println("You chose to buy " + quantity[0] + " Supreme hoodie\n" );
 			}
 			else if(itemNum == 2) {//if user picks 2, they choose that item
 				price += calcCost(priceArray[itemNum - 1], qty);//calculates the total cost 
 				quantity[itemNum-1] += qty;
-				System.out.println("You chose to buy " + quantity[0] + " Bape shark hoodie");
+				System.out.println("You chose to buy " + quantity[0] + " Bape hoodie\n");
 			}
 			else if(itemNum == 3) {//if user picks 3, they choose that item
 				quantity[itemNum-1] += qty;
 				price += calcCost(priceArray[itemNum - 1], qty);//calculates the total cost
 
-				System.out.println("You chose to buy " + quantity[0] + " Louis V x Supreme collab duffle bag");
+				System.out.println("You chose to buy " + quantity[0] + " Louis V bag\n");
 			}
 			else if(itemNum == 4) {//if user picks 4, they choose that item
 				quantity[itemNum-1] += qty;
 				price += calcCost(priceArray[itemNum - 1], qty);//calculates the total cost 
 
-				System.out.println("You chose to buy " + quantity[0] + " Gucci flip flops");
+				System.out.println("You chose to buy " + quantity[0] + " Gucci flip flops\n");
 			}
 			else if(itemNum == 5) {//if user picks 5, they choose that item
 				quantity[itemNum-1] += qty;
 				price += calcCost(priceArray[itemNum - 1], qty);//calculates the total cost 
 
-				System.out.println("You chose to buy " + quantity[0] + " Balenciaga Triple S sneakers");
+				System.out.println("You chose to buy " + quantity[0] + " Balenciaga shoes\n");
 			}
 			else if(itemNum == 6) {//if user picks 5, they choose that item
 				quantity[itemNum-1] += qty;
 				price += calcCost(priceArray[itemNum - 1], qty);//calculates the total cost 
 		
-				System.out.println("You chose to buy " + quantity[0] + " OffWhite x Nike Jordan 1's");
+				System.out.println("You chose to buy " + quantity[0] + " Off-White belt\n");
 			}
 			else if(itemNum == 7) {//if user picks 5, they choose that item
 				quantity[itemNum-1] += qty;
 				price += calcCost(priceArray[itemNum - 1], qty);//calculates the total cost 
 			
-				System.out.println("You chose to buy " + quantity[0] + " Yeezy 350 boosts");
+				System.out.println("You chose to buy " + quantity[0] + " Yeezy 350 V2\n");
+			}
+			else if(itemNum == 8) {//if user picks 5, they choose that item
+				quantity[itemNum-1] += qty;
+				price += calcCost(priceArray[itemNum - 1], qty);//calculates the total cost 
+			
+				System.out.println("You chose to buy " + quantity[0] + " Nike AirMax\n");
+			}
+			else if(itemNum == 9) {//if user picks 5, they choose that item
+				quantity[itemNum-1] += qty;
+				price += calcCost(priceArray[itemNum - 1], qty);//calculates the total cost 
+			
+				System.out.println("You chose to buy " + quantity[0] + " Adidas NMD\n");
+			}
+			else if(itemNum == 10) {//if user picks 5, they choose that item
+				quantity[itemNum-1] += qty;
+				price += calcCost(priceArray[itemNum - 1], qty);//calculates the total cost 
+			
+				System.out.println("You chose to buy " + quantity[0] + " Nike Vapormax\n");
 			}
 			else {//this runs when the user picks something that is not in the shop
-				System.out.println("Sorry we don't got that, come again next time");//prints this to the console
+				System.out.println("Sorry we don't got that, come again next time\n");//prints this to the console
 			}
 			quantity[itemNum-1] += qty;
 		}//end if (itemNum != 0)
